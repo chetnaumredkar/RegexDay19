1de6326373566d62416fb6fdfe4096af16b5a82b
kolhechetna <kolhe.chetna@gmail.com>
2022-12-07 15:57:30 +0530

    Added:valid first name

--- src/com/bridgelabz/ValidFirstName.java
+++ src/com/bridgelabz/ValidFirstName.java
@@ -3,26 +3,26 @@ package com.bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidFirstName {
+public class ValidFirstName{
 	public static boolean checkValidFirstName(String firstName) {
-		String firstNameregex="[A-Z][a-z]{3,}";
-		Pattern pattern=Pattern.compile(firstNameregex);
-	      if(firstName==null) {
+		String firstNameregex = "[A-Z][a-z]{3,}";
+		Pattern pattern = Pattern.compile(firstNameregex);
+		if (firstName == null) {
 			return false;
 		}
-	      Matcher matcher=pattern.matcher(firstName);
-	      return matcher.matches();
-		
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+
 	}
+
 	public static void main(String[] args) {
-		String firstName="juuu";
-		boolean isfirstNameValid=checkValidFirstName(firstName);
-		if(isfirstNameValid) {
-			System.out.println(firstName+" is valid");}
-		else {
-			System.out.println(firstName+" is not valid");}
-
+		String firstName = "juuu";
+		boolean isfirstNameValid = checkValidFirstName(firstName);
+		if (isfirstNameValid) {
+			System.out.println(firstName + " is valid");
+		} else {
+			System.out.println(firstName + " is not valid");
 		}
+
 	}
-
-
+}
